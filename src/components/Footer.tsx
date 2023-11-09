a319e47618c14cf7c38916012a1ec778e383581e
Davidson George <daviiiwilliams@live.com>
2023-11-09 07:23:10 -0300

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer: React.FC = () => {
         <footer>
 
             <p>BZ97 Via Park - Outback  &copy; <span id="anoAtual"> 2023</span> </p>
-            <p>Versão: 1.00A </p>
+            <p>Versão: 1.01c <blockquote>07210/911200-3</blockquote> </p>
             
         </footer>
     );
