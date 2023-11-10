96f6db04075e8c17306bd255fa84fa77a00bf58a
Davidson George <daviiiwilliams@live.com>
2023-11-10 00:28:35 -0300

    002810112023

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,14 +3,18 @@ import React from 'react';
 const Footer: React.FC = () => {
     return (
         <footer>
-
-            <p>BZ97 Via Park - Outback  &copy; <span id="anoAtual"> 2023</span> </p>
-            <p>Versão: 1.02b <blockquote>20410/911200-3</blockquote> </p>
-            <p>UPDATE: <blockquote>09/11</blockquote> </p>
-
-            <div>
+            <div className='link-ranking'>
                 <img src="https://dagesico.pythonanywhere.com/static/img/qrcode.png" alt="QR Code Ranking" />
             </div>
+
+            <div className='link-ranking'>
+                <p>BZ97 Via Park - Outback  &copy; <span id="anoAtual"> 2023</span> </p>
+                <p>Versão: 1.02b <blockquote>20410/911200-3</blockquote> </p>
+                <p>UPDATE: <blockquote>09/11</blockquote> </p>
+            </div>
+
+
+
 
         </footer>
     );
