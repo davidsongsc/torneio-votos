16d92780fa2704b4752f2a5705c3818e892efdac
Davidson George <daviiiwilliams@live.com>
2023-11-10 00:20:44 -0300

    002010112023

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,11 @@ const Footer: React.FC = () => {
             <p>BZ97 Via Park - Outback  &copy; <span id="anoAtual"> 2023</span> </p>
             <p>Versão: 1.02b <blockquote>20410/911200-3</blockquote> </p>
             <p>UPDATE: <blockquote>09/11</blockquote> </p>
-            
+
+            <div>
+                <img src="https://dagesico.pythonanywhere.com/static/img/qrcode.png" alt="QR Code Ranking" />
+            </div>
+
         </footer>
     );
 };
