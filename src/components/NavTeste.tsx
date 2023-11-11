9d197a74d95f7e585be7acd1f8c2ec4cf9ef938f
Davidson George <53156529+davidsongsc@users.noreply.github.com>
2023-11-11 18:05:08 -0300

    Atualizar o NavTeste.tsx

--- src/components/NavTeste.tsx
+++ src/components/NavTeste.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const NavPrep: React.FC = () => {
     return (
-        <nav style={{ position: 'fixed', top: '0', left: '0', right: '0', zIndex: '999', backgroundColor: 'red', color: 'white', textAlign: 'center', fontSize: '11px', letterSpacing: '15px' }}>
+        <nav style={{ position: 'fixed', top: '0', left: '0', right: '0', zIndex: '999', backgroundColor: 'red', color: 'white', textAlign: 'center', fontSize: '11px', letterSpacing: '8px' }}>
             <h1>MODO DE TESTE</h1>
         </nav>
     );
