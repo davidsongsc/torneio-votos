ef1bcfbb36e7237b6ae69b2d8799ecb47a936df3
Davidson George <daviiiwilliams@live.com>
2023-11-11 05:09:00 -0300

    1.03g

--- src/components/Votar.tsx
+++ src/components/Votar.tsx
@@ -73,6 +73,8 @@ const Votar: React.FC = () => {
 
     const handleCancel = () => {
         setShowConfirmDialog(false);
+        dispatch(votar(0));
+
     };
 
     const filteredPessoas = pessoas.filter((pessoa) =>
@@ -89,11 +91,6 @@ const Votar: React.FC = () => {
 
     return (
         <div>
-            <h2 style={{padding: '13px 0'}}>
-                <strong style={{ fontSize: '30px', fontFamily: 'Times New Roman', backgroundColor: 'rgb(32, 39, 68)',borderStyle: 'groove', padding: '6px', borderRadius:'33px'}}>
-                    {userLogin.userInfo?.votos}
-                </strong>
-            </h2>
 
             <form className='form-votacao-indica' >
                 <input
@@ -108,7 +105,10 @@ const Votar: React.FC = () => {
                 <div className='tela-painel-votos'>
                     {top3Pessoas.map((pessoa) => (
                         <div key={pessoa.id} onClick={() => handleVotoChange(pessoa.id)}>
-                            <label className={`radio-label ${pessoa.id === voto ? 'radio-label-selecionado' : 'radio-label-nao-selecionado'} ${pessoa.id === voto && showConfirmDialog ? 'radio-label-voto' : 'radio-label-voto-nao'}`} htmlFor={`radio-${pessoa.id}`} style={{ display: `${pessoa.id === usuarioLogado ? 'none' : ''}` }}>
+                            <label
+                                className={`radio-label ${pessoa.id === voto ? 'radio-label-selecionado' : 'radio-label-nao-selecionado'} ${pessoa.id === voto && showConfirmDialog ? 'radio-label-voto' : 'radio-label-voto-nao'}`}
+                                htmlFor={`radio-${pessoa.id}`}
+                                style={{ display: `${pessoa.id === usuarioLogado ? 'none' : ''}` }}>
                                 <div className='votante-lista-seletor'>
                                     <img src={`https://dagesico.pythonanywhere.com/static/img/${pessoa.imagem}`} alt="Imagem Perfil" />
                                     <h3>{pessoa.nome} </h3>
@@ -119,17 +119,29 @@ const Votar: React.FC = () => {
                                         id={`radio-${pessoa.id}`}
                                         value={pessoa.id}
                                         checked={voto === pessoa.id}
+                                        onChange={() => handleVotoChange(pessoa.id)}
                                     />
-                                    <div className={`radio-custom${voto === pessoa.id ? ' radio-checked' : ''}`} />
+                                    <div className={`radio-custom ${pessoa.id === voto ? ' radio-checked' : ' radio-not-checked'}`} />
                                 </div>
                             </label>
                         </div>
                     ))}
-
                 </div>
-                <button className='btn-voto' type="button" onClick={handleVotar} style={{ display: `${!showConfirmDialog ? '' : 'none'}` }}>
-                    Votar
-                </button>
+                <div style={{
+                    position: 'relative',
+                    zIndex: '133',
+                }}>
+
+                    <h2 style={{ padding: '13px 0' }}>
+                        <strong style={{ fontSize: '30px', fontFamily: 'Times New Roman', backgroundColor: 'rgb(32, 39, 68)', borderStyle: 'groove', padding: '6px', borderRadius: '33px' }}>
+                            {userLogin.userInfo?.votos}
+                        </strong>
+                    </h2>
+                    <button className='btn-voto' type="button" onClick={handleVotar} style={{ display: `${!showConfirmDialog ? '' : 'none'}` }}>
+                        Votar
+                    </button>
+                </div>
+
             </form>
             <ConfirmVoto
                 open={showConfirmDialog}
