d18c93b3085c148b84727f4e955c56ea193d0300
Davidson George <daviiiwilliams@live.com>
2023-11-13 14:47:12 -0300

    1.04c

--- src/reducers/configReducer.ts
+++ src/reducers/configReducer.ts
@@ -8,7 +8,7 @@ interface UserState {
 const initialState: UserState = {
   config: [
     {
-      ver: '1.04b',
+      ver: '1.04c',
       valendo: false,
       unidade: 'bz97',
       prazoManutencao: '2023-11-13T01:56:00',
